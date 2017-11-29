2ebe9b40dee1b5897b81beea5c75534e19d78bd7
Jesse <jesse@jessechapman.com.au>
2017-11-29 21:41:08 +1100

    got hover working, need to fix titles for them and first box

--- hover.js
+++ hover.js
@@ -1,10 +1,35 @@
 $(document).ready( function() {
-    function handler( event ) {
-    var target = $( event.target );
-    if ( target.is( ".flex-box" ) ) {
-      target.children().fadeIn(200);
-    } else $(".flex-box-cont").fadeOut(200);
-  }
-  $( ".flex-box" ).mouseenter( handler );
-  $( ".flex-box" ).children().mouseleave( handler );
-});
+//FIRST DIV OF TARGET
+  
+
+
+$('.toggle').mouseenter( function() {
+    $('.flex-box-cont', this).fadeIn(200);
+})
+$('.toggle').mouseleave( function() {
+    $('.flex-box-cont', this).fadeOut(200);
+})
+
+
+
+
+
+//     function handler( event ) {
+//     var target = $( event.target );
+//     if ( target.is( ".flex-box" ) ) {
+//       target.children().fadeToggle(200);
+//   } else target.children(':first').fadeToggle(200);
+//   }
+//   $( ".flex-box" ).mouseenter( handler );
+//   $( ".flex-box-cont" ).mouseleave( handler );
+
+
+  //   function handler( event ) {
+  //   var target = $( event.target );
+  //   if ( target.is( ".flex-box" ) ) {
+  //     target.children().fadeToggle(200);
+  // } else $(".flex-box").fadeToggle(200);
+  // }
+  // $( ".flex-box" ).mouseenter( handler );
+  // $( ".flex-box" ).children().mouseleave( handler );
+ });
