b9a323eee110bf85c8805bf881c97da12edb8f24
Jesse <jesse@jessechapman.com.au>
2017-12-05 12:23:23 +1100

    added local jquery, all hovers now working

--- hover.js
+++ hover.js
@@ -1,13 +1,11 @@
 $(document).ready( function() {
-//FIRST DIV OF TARGET
-  
 
 
 $('.toggle').mouseenter( function() {
-    $('.flex-box-cont', this).fadeIn(200);
+    $('.flex-box-cont', this).fadeIn(150);
 })
 $('.toggle').mouseleave( function() {
-    $('.flex-box-cont', this).fadeOut(200);
+    $('.flex-box-cont', this).fadeOut(150);
 })
 
 
